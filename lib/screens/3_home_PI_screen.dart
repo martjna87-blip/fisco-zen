7d03928ce0b677ff92a91e781f8439f9b49b4da4
IlTuoNome <la_tua_email_personale@gmail.com>
2026-07-31 10:33:09 +0200

    Aggiornamento

--- lib/screens/3_home_PI_screen.dart
+++ lib/screens/3_home_PI_screen.dart
@@ -569,8 +569,12 @@ class _HomeScreenState extends State<HomeScreen> {
                   children: [
                     const SizedBox(height: 0), // 👈 SPAZIO RIMOSSO PER ALZARE IL TESTO
                     const Text(
-                      'Portafoglio netto', 
-                      style: TextStyle(color: Colors.white70, fontSize: 14, fontWeight: FontWeight.w400),
+                      'Gestione P.IVA',
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                      ),
                     ),
                     const SizedBox(height: 6),
                     Row(
